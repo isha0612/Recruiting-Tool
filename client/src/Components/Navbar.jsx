569070ab0cc3cefc137995b185795bcabc8267b6
isha0612 <isha06dec@gmail.com>
2024-03-21 02:06:05 +0530

    enhancing ui

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -33,7 +33,7 @@ export default function Navbar({ page }) {
                     <svg className="fill-current h-8 w-8 mr-2 dark:text-gray-900" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" /></svg>
                     <span className="font-semibold text-xl tracking-tight dark:text-gray-900">Recruiting Tool</span>
                 </div>
-                {page === "auth" && <DarkModeSwitch className="mx-auto sm:inline-block sm:mr-4" id="theme-switch" onChange={toggleDarkMode} checked={isDarkMode === 'false' ? true : false} size={30} />}
+                {page === "auth" && <DarkModeSwitch className="mr-0 sm:inline-block sm:mr-4" id="theme-switch" onChange={toggleDarkMode} checked={isDarkMode === 'false' ? true : false} size={30} />}
                 {page !== "auth" &&
                     <>
                         <div className="inline-block mt-2 lg:hidden sm:mb-0">
@@ -42,12 +42,12 @@ export default function Navbar({ page }) {
                             </button>
                         </div>
                         <div className={`w-full block flex-grow justify-end lg:flex lg:items-center lg:w-auto ${isMenuOpen ? 'block' : 'hidden'}`}>
-                            <DarkModeSwitch className="mr-4 mx-2 mt-4 mb-2 sm:inline-block" id="theme-switch" onChange={toggleDarkMode} checked={isDarkMode === 'false' ? true : false} size={30} />
-                            {page === "candidate" && <Link to="/home" className="sm:inline-block text-sm px-2 py-1.5 mr-4 leading-none border rounded text-white border-white hover:border-transparent hover:text-blue-500 hover:bg-white mt-4 lg:mt-0 dark:text-gray-900 dark:border-gray-900 dark:hover:text-white dark:hover:bg-gray-700 sm:text-base sm:px-4 py-2">
+                            <DarkModeSwitch className="sm:inline-block md:inline-block block mr-4 md:mb-4 sm:inline-block mt-2" id="theme-switch" onChange={toggleDarkMode} checked={isDarkMode === 'false' ? true : false} size={30} />
+                            {page === "candidate" && <Link to="/home" className="sm:inline-block md:inline-block text-sm px-2 py-1.5 mr-4 leading-none border rounded text-white border-white hover:border-transparent hover:text-blue-500 hover:bg-white mt-4 lg:mt-0 dark:text-gray-900 dark:border-gray-900 dark:hover:text-white dark:hover:bg-gray-700 sm:text-base sm:px-4 py-2">
                                 Home
                             </Link>}
-                            {page === "" && <Link to="/add_candidate" className="sm:inline-block text-sm px-2 py-1.5 mr-4 leading-none border rounded text-white border-white hover:border-transparent hover:text-blue-500 hover:bg-white mt-4 lg:mt-0 dark:text-gray-900 dark:border-gray-900 dark:hover:text-white dark:hover:bg-gray-700 sm:text-base sm:px-4 py-2">Add a Candidate</Link>}
-                            {page !== "auth" && <div onClick={handleLogout} className="hover:cursor-pointer inline-block text-sm px-2 py-1.5 mr-4 leading-none border rounded text-white border-white hover:border-transparent hover:text-blue-500 hover:bg-white mt-4 lg:mt-0 dark:text-gray-900 dark:border-gray-900 dark:hover:text-white dark:hover:bg-gray-700 sm:text-base">Log Out</div>}
+                            {page === "" && <Link to="/add_candidate" className="sm:inline-block md:inline-block text-sm px-2 py-1 mr-4 leading-none border rounded text-white border-white hover:border-transparent hover:text-blue-500 hover:bg-white mt-4 lg:mt-0 dark:text-gray-900 dark:border-gray-900 dark:hover:text-white dark:hover:bg-gray-700 sm:text-base sm:px-4 py-2">Add a Candidate</Link>}
+                            {page !== "auth" && <div onClick={handleLogout} className="hover:cursor-pointer inline-block md:inline-block text-sm px-2 py-2 mr-4 leading-none border rounded text-white border-white hover:border-transparent hover:text-blue-500 hover:bg-white mt-4 lg:mt-0 dark:text-gray-900 dark:border-gray-900 dark:hover:text-white dark:hover:bg-gray-700 sm:text-base">Log Out</div>}
                         </div>
                     </>
                 }
